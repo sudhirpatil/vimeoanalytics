c3f7e7df0d7eee5ce349f81cccd880c0306e5c65
sudhirpatil <spatil.sudhir@gmail.com>
2019-07-26 09:59:42 +0800

    dynamodb test code

--- src/main/java/com/sp/parrot/DynamoClient.java
+++ src/main/java/com/sp/parrot/DynamoClient.java
@@ -13,11 +13,11 @@ import io.vertx.ext.sql.SQLConnection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.ListTablesRequest;
-import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
 import java.util.List;
 
 public class DynamoClient {
@@ -33,46 +33,46 @@ public class DynamoClient {
     {
         System.out.println("Your DynamoDB tables:\n");
 
-//        final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
-//
-//        ListTablesRequest request;
-//
-//        boolean more_tables = true;
-//        String last_name = null;
-//
-//        while(more_tables) {
-//            try {
-//                if (last_name == null) {
-//                    request = new ListTablesRequest().withLimit(10);
-//                }
-//                else {
-//                    request = new ListTablesRequest()
-//                            .withLimit(10)
-//                            .withExclusiveStartTableName(last_name);
-//                }
-//
-//                ListTablesResult table_list = ddb.listTables(request);
-//                List<String> table_names = table_list.getTableNames();
-//
-//                if (table_names.size() > 0) {
-//                    for (String cur_name : table_names) {
-//                        System.out.format("* %s\n", cur_name);
-//                    }
-//                } else {
-//                    System.out.println("No tables found!");
-//                    System.exit(0);
-//                }
-//
-//                last_name = table_list.getLastEvaluatedTableName();
-//                if (last_name == null) {
-//                    more_tables = false;
-//                }
-//
-//            } catch (AmazonServiceException e) {
-//                System.err.println(e.getErrorMessage());
-//                System.exit(1);
-//            }
-//        }
+        // snippet-start:[dynamodb.java2.list_tables.main]
+        DynamoDbClient ddb = DynamoDbClient.create();
+
+        boolean more_tables = true;
+        String last_name = null;
+
+        while(more_tables) {
+            try {
+                ListTablesResponse response = null;
+                if (last_name == null) {
+                    ListTablesRequest request = ListTablesRequest.builder().build();
+                    response = ddb.listTables(request);
+                }
+                else {
+                    ListTablesRequest request = ListTablesRequest.builder()
+                            .exclusiveStartTableName(last_name).build();
+                    response = ddb.listTables(request);
+                }
+
+                List<String> table_names = response.tableNames();
+
+                if (table_names.size() > 0) {
+                    for (String cur_name : table_names) {
+                        System.out.format("* %s\n", cur_name);
+                    }
+                } else {
+                    System.out.println("No tables found!");
+                    System.exit(0);
+                }
+
+                last_name = response.lastEvaluatedTableName();
+                if (last_name == null) {
+                    more_tables = false;
+                }
+            } catch (DynamoDbException e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
+        }
+        // snippet-end:[dynamodb.java2.list_tables.main]
         System.out.println("\nDone!");
     }
 }
